e133801da64d8c27bb98d0bff508b16fea557d68
Donovan Glover <donovan@dglover.co>
2023-08-04 00:05:49 -0400

    flake.nix: Source from repository

--- flake.nix
+++ flake.nix
@@ -1,21 +1,26 @@
 {
-  description = "Generate directory thumbnails from images inside them.";
+  description = "Generate directory thumbnails from images inside them";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
+    packages.x86_64-linux.default = buildGoModule {
+      name = "go-thumbnailer";
+      src = ./.;
+
+      buildInputs = with pkgs; [ vips ];
+      nativeBuildInputs = with pkgs; [ pkg-config ];
+
+      vendorSha256 = "sha256-NuosYDNDiygPArKLYrLg4jEjQhTumdZ/5GDy4JY4F9s=";
+
+      postInstall = ''
+        mkdir -p $out/share/thumbnailers
+        substituteAll ${./go.thumbnailer} $out/share/thumbnailers/go.thumbnailer
+      '';
+    };
+
+    devShells.default = pkgs.mkShell {
+      buildInputs = with pkgs; [ vips ];
+      nativeBuildInputs = with pkgs; [ pkg-config ];
+    };
   };
-
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages = {
-          default = ./.;
-        };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ go ];
-        };
-      });
 }
