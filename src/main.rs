56cc336b08e3e0e57f71d605f19a8285941e9c38
Donovan Glover <donovan@dglover.co>
2023-08-04 01:26:56 -0400

    libvips: Add compiling code that panics
    No idea why this panics however it doesn't panic when custom options
    aren't used. It may be possible to manually edit the bindings to change
    the default option and get what I want.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,17 @@
 use libvips::{ops, VipsImage, VipsApp};
+use libvips::ops::Interesting;
 
 fn main() {
-    // this initializes the libvips library. it has to live as long as the application lives (or as long as you want to use the library within your app)
-    // you can't have multiple objects of this type and when it is dropped it will call the libvips functions to free all internal structures.
     let app = VipsApp::new("Test Libvips", false).expect("Cannot initialize libvips");
-    //set number of threads in libvips's threadpool
+
     app.concurrency_set(2);
-    // loads an image from file
+
     let image = VipsImage::new_from_file("cover.png").unwrap();
 
-    // will resized the image and return a new instance.
-    // libvips works most of the time with immutable objects, so it will return a new object
-    // the VipsImage struct implements Drop, which will free the memory
-    let resized = ops::resize(&image, 0.5).unwrap();
-
-    //optional parameters
-    let options = ops::JpegsaveOptions {
-        q: 90,
-        background: vec![255.0],
-        strip: true,
-        optimize_coding: true,
-        optimize_scans: true,
-        interlace: true,
-        ..ops::JpegsaveOptions::default()
+    let options = ops::ThumbnailImageOptions {
+        crop: Interesting::Centre,
+        ..ops::ThumbnailImageOptions::default()
     };
 
-    // alternatively you can use `jpegsave` that will use the default options
-    match ops::jpegsave_with_opts(&resized, "output.jpg",  &options) {
-        Err(_) => println!("error: {}", app.error_buffer().unwrap()),
-        Ok(_) => println!("Great Success!")
-    }
+    ops::thumbnail_image_with_opts(&image, 128, &options).unwrap();
 }
