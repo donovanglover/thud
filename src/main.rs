b1690a4fc0581ce257af70a4bba81050654ea5ab
Donovan Glover <donovan@dglover.co>
2023-08-04 03:17:42 -0400

    meta: Replace libvips with image crate
    The image crate seems to be the most popular crate for images in Rust.
    Additionally, the dependency on libvips is no longer needed.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use libvips::{ops, VipsImage, VipsApp};
+use image::*;
+use imageops::FilterType;
 
 fn main() {
-    let app = VipsApp::new("Test Libvips", false).expect("Cannot initialize libvips");
+    // Use the open function to load an image from a Path.
+    // `open` returns a `DynamicImage` on success.
+    let img = image::open("cover.png").unwrap();
+    let new_img = DynamicImage::resize_to_fill(&img, 512, 512, FilterType::Nearest);
 
-    app.concurrency_set(2);
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", new_img.dimensions());
 
-    let image = VipsImage::new_from_file("cover.png").unwrap();
-
-    let thumbnail = ops::thumbnail_image(&image, 512).unwrap();
-
-    match ops::vipssave(&thumbnail, "output.png") {
-        Err(_) => println!("error: {}", app.error_buffer().unwrap()),
-        Ok(_) => println!("Great Success!")
-    }
+    // Write the contents of this image to the Writer in PNG format.
+    new_img.save("test.png").unwrap();
 }
