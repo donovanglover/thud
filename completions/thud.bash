308932687c11c811a3467e795a878eb606576dfe
Donovan Glover <donovan@dglover.co>
2023-08-08 11:27:29 -0400

    feat: Add shell completions

---
+++ completions/thud.bash
@@ -0,0 +1,62 @@
+_thud() {
+    local i cur prev opts cmd
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cmd=""
+    opts=""
+
+    for i in ${COMP_WORDS[@]}
+    do
+        case "${cmd},${i}" in
+            ",$1")
+                cmd="thud"
+                ;;
+            *)
+                ;;
+        esac
+    done
+
+    case "${cmd}" in
+        thud)
+            opts="-s -i -o -h -V --size --input-directory --output-file --help --version"
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                --size)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -s)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --input-directory)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -i)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                --output-file)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                -o)
+                    COMPREPLY=($(compgen -f "${cur}"))
+                    return 0
+                    ;;
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+    esac
+}
+
+complete -F _thud -o bashdefault -o default thud
---
+++ completions/thud.fish
@@ -0,0 +1,5 @@
+complete -c thud -s s -l size -r
+complete -c thud -s i -l input-directory -r -F
+complete -c thud -s o -l output-file -r -F
+complete -c thud -s h -l help -d 'Print help (see more with \'--help\')'
+complete -c thud -s V -l version -d 'Print version'
