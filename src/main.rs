c3b363a6f47132d963f9a31a98c9a43c05ec0766
Donovan Glover <donovan@dglover.co>
2023-08-08 09:51:56 -0400

    meta: Update main logic
    This should work if the input_directory is a valid directory, which we
    should be able to check for with Rust types.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use clap::Parser;
+use cli::Cli;
 use image::*;
 use imageops::FilterType;
 
+mod cli;
+
 fn main() {
+    let Cli { size, input_directory, output_file } = Cli::parse();
+
     // Use the open function to load an image from a Path.
     // `open` returns a `DynamicImage` on success.
-    let img = image::open("cover.png").unwrap();
-    let new_img = DynamicImage::resize_to_fill(&img, 512, 512, FilterType::Nearest);
-
-    // The dimensions method returns the images width and height.
-    println!("dimensions {:?}", new_img.dimensions());
+    let img = image::open(input_directory + "/cover.png").unwrap();
+    let new_img = DynamicImage::resize_to_fill(&img, size, size, FilterType::Nearest);
 
     // Write the contents of this image to the Writer in PNG format.
-    new_img.save("test.png").unwrap();
+    new_img.save(output_file).unwrap();
 }
