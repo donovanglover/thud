73b6e2221ccdd9e579356d6d737b2f104db733a2
Donovan Glover <donovan@dglover.co>
2023-08-08 09:58:53 -0400

    chore: Convert PathBuf to String

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,11 @@ fn main() {
 
     // Use the open function to load an image from a Path.
     // `open` returns a `DynamicImage` on success.
-    let img = image::open(input_directory + "/cover.png").unwrap();
-    let new_img = DynamicImage::resize_to_fill(&img, size, size, FilterType::Nearest);
+    if let Some(input_directory) = input_directory.to_str() {
+        let img = image::open(input_directory.to_owned() + "/cover.png").unwrap();
+        let new_img = DynamicImage::resize_to_fill(&img, size, size, FilterType::Nearest);
 
-    // Write the contents of this image to the Writer in PNG format.
-    new_img.save(output_file).unwrap();
+        // Write the contents of this image to the Writer in PNG format.
+        new_img.save(output_file).unwrap();
+    }
 }
