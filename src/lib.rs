29d4f4e3a56d572c461d57da0446c7b5dff4f810
Donovan Glover <donovan@dglover.co>
2023-08-14 18:41:17 -0400

    log: Make it easier to differentiate success

--- src/lib.rs
+++ src/lib.rs
@@ -86,7 +86,7 @@ pub mod strategy {
             let potential_image = input_directory.to_owned() + "/" + &file;
 
             if let Ok(img) = image::open(&potential_image) {
-                log(&("Matched ".to_owned() + &input_directory + " with " + &potential_image));
+                log(&("SUCCESS: Using ./".to_owned() + &file + " for " + &input_directory + "/"));
 
                 DynamicImage::resize_to_fill(&img, size, size, filter).save(output_file).unwrap();
 
