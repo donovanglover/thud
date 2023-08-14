649b35b4dc23427b949ae2015bb00b5324f0a18b
Donovan Glover <donovan@dglover.co>
2023-08-14 18:53:54 -0400

    chore: Run cargo fmt on tests

--- tests/config.rs
+++ tests/config.rs
@@ -2,15 +2,24 @@ use thud::get_config;
 
 #[test]
 fn get_config_returns_none_if_file_dne() {
-    assert!(get_config("this-file-does-not-exist.toml").is_none(), "get_config() returns None if the file does not exist");
+    assert!(
+        get_config("this-file-does-not-exist.toml").is_none(),
+        "get_config() returns None if the file does not exist"
+    );
 }
 
 #[test]
 fn get_config_returns_none_if_invalid_toml() {
-    assert!(get_config("./examples/invalid.toml").is_none(), "get_config() returns None if the toml is invalid");
+    assert!(
+        get_config("./examples/invalid.toml").is_none(),
+        "get_config() returns None if the toml is invalid"
+    );
 }
 
 #[test]
 fn get_config_returns_config() {
-    assert!(thud::get_config("./examples/config.toml").is_some(), "get_config() returns a config if the file exists and is valid");
+    assert!(
+        thud::get_config("./examples/config.toml").is_some(),
+        "get_config() returns a config if the file exists and is valid"
+    );
 }
--- tests/filter.rs
+++ tests/filter.rs
@@ -1,12 +1,20 @@
-use thud::get_filter;
 use image::imageops::FilterType;
+use thud::get_filter;
 
 #[test]
 fn get_filter_returns_default_filter() {
-    assert_eq!(get_filter("this-filter-dne"), FilterType::Lanczos3, "get_filter() returns default filter as Lanczos3");
+    assert_eq!(
+        get_filter("this-filter-dne"),
+        FilterType::Lanczos3,
+        "get_filter() returns default filter as Lanczos3"
+    );
 }
 
 #[test]
 fn get_filter_matches_random_caps() {
-    assert_eq!(get_filter("NeAResT"), FilterType::Nearest, "get_filter() disregards how a filter is capitalized");
+    assert_eq!(
+        get_filter("NeAResT"),
+        FilterType::Nearest,
+        "get_filter() disregards how a filter is capitalized"
+    );
 }
