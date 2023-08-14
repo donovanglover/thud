6a27aee149d1bd0c4c8ebf80ed5872301eded9bc
Donovan Glover <donovan@dglover.co>
2023-08-14 12:27:17 -0400

    chore: Remove useless comment

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ mod cli;
 fn main() {
     let Cli { size, input_directory, output_file } = Cli::parse();
 
-    // Use the open function to load an image from a Path.
-    // `open` returns a `DynamicImage` on success.
     if let Some(input_directory) = input_directory.to_str() {
         if let Ok(img) = image::open(input_directory.to_owned() + "/cover.png") {
             DynamicImage::resize_to_fill(&img, size, size, FilterType::Nearest).save(output_file).unwrap();
