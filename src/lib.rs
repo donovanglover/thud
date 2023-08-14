d8add4fea175d84be2fd1bb3857e83c08b7509c8
Donovan Glover <donovan@dglover.co>
2023-08-14 18:35:04 -0400

    strategy(cover): Log when image chosen

--- src/lib.rs
+++ src/lib.rs
@@ -77,13 +77,19 @@ pub mod strategy {
     use image::DynamicImage;
     use image::imageops::FilterType;
     use crate::cli::Cli;
+    use crate::log;
 
     pub fn cover(input_directory: String, files: Vec<String>, filter: FilterType) {
         let Cli { size, output_file, .. } = Cli::parse();
 
         for file in files {
-            if let Ok(img) = image::open(input_directory.to_owned() + "/" + &file) {
+            let potential_image = input_directory.to_owned() + "/" + &file;
+
+            if let Ok(img) = image::open(&potential_image) {
+                log(&("Matched ".to_owned() + &input_directory + " with " + &potential_image));
+
                 DynamicImage::resize_to_fill(&img, size, size, filter).save(output_file).unwrap();
+
                 return
             }
         }
