e11e7c92d85ff9362a8d85da5113eb6c9bb820bd
Donovan Glover <donovan@dglover.co>
2023-08-14 18:34:02 -0400

    log: Make info easier to notice

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
     if let Some(input_directory_str) = input_directory.to_str() {
         if let Some(config) = get_home_config() {
-            log("info: using ~/.config/thud/config.toml");
+            log("INFO: Using ~/.config/thud/config.toml");
 
             if let Some(rules) = config.rules {
                 for rule in rules {
@@ -33,7 +33,7 @@ fn main() {
             return
         }
 
-        log("info: using default cover.{png,jpg}");
+        log("INFO: Using default cover.{png,jpg}");
 
         strategy::cover(input_directory_str.to_owned(), vec!["cover.png".to_string(), "cover.jpg".to_string()], get_filter("lanczos3"));
     }
