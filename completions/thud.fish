1bc25763e2a27c14f860968e12e74ca73b09d03e
Donovan Glover <donovan@dglover.co>
2023-08-14 17:12:59 -0400

    chore: Update shell completions

--- completions/thud.fish
+++ completions/thud.fish
@@ -1,5 +1,5 @@
-complete -c thud -s s -l size -r
-complete -c thud -s i -l input-directory -r -F
-complete -c thud -s o -l output-file -r -F
+complete -c thud -s s -l size -d 'Size of the thumbnail to output' -r
+complete -c thud -s i -l input-directory -d 'Directory to base the thumbnail off of' -r -F
+complete -c thud -s o -l output-file -d 'Where to save the output image' -r -F
 complete -c thud -s h -l help -d 'Print help (see more with \'--help\')'
 complete -c thud -s V -l version -d 'Print version'
