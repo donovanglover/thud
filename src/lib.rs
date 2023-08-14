971fba1dcf38e05838909da6e22ffc6f9879e441
Donovan Glover <donovan@dglover.co>
2023-08-14 15:26:04 -0400

    lib: Use option instead of default config
    Should improve performance.

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,6 @@ use std::fs;
 use std::path::Path;
 use serde::Deserialize;
 
-const DEFAULT_CONFIG: &str = r#"
-[[rules]]
-path = "/"
-strategy = "cover"
-files = [
-  "cover.png",
-  "cover.jpg"
-]
-"#;
-
 #[derive(Debug, Deserialize)]
 pub struct Config {
     rules: Option<Vec<Rules>>,
@@ -25,10 +15,10 @@ pub struct Rules {
     filter: Option<String>,
 }
 
-pub fn get_config(file: &Path) -> Config {
+pub fn get_config(file: &Path) -> Option<Config> {
     if let Ok(config) = fs::read_to_string(file) {
         toml::from_str(config.as_str()).unwrap()
     } else {
-        toml::from_str(DEFAULT_CONFIG).unwrap()
+        None
     }
 }
